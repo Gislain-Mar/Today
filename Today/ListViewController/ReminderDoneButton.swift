3cb450f2bcdd54daf2e59f99d048f75360721d47
Gislain Marlhens <gislainmarlhens@macbook-air-de-gislain.home>
2023-11-28 19:15:01 +0100

    [NEW] création du projet

---
+++ Today/ListViewController/ReminderDoneButton.swift
@@ -0,0 +1,12 @@
+//
+//  ReminderDoneButton.swift
+//  Today
+//
+//  Created by Gislain Marlhens on 20/11/2023.
+//
+
+import UIKit
+
+class ReminderDoneButton: UIButton {
+    var id: Reminder.ID?
+}
