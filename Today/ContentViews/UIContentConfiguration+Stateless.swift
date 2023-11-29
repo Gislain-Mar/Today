bfeb2d718df684a1170ffdd6133a4edbf06c956d
Gislain Marlhens <gislainmarlhens@MacBook-Air-de-Gislain.local>
2023-11-29 13:41:12 +0100

    [NEW] Rappels : rendre les rappels éditables

---
+++ Today/ContentViews/UIContentConfiguration+Stateless.swift
@@ -0,0 +1,15 @@
+//
+//  UIContentConfiguration+Stateless.swift
+//  Today
+//
+//  Created by Gislain Marlhens on 28/11/2023.
+//
+
+import UIKit
+
+
+extension UIContentConfiguration {
+    func updated(for state: UIConfigurationState) -> Self {
+        return self
+    }
+}
